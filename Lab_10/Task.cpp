efbeb3562d9566a385eb89451253239c737752cb
Sarim Ahmed <88588593+thenoisyninga@users.noreply.github.com>
2024-04-07 20:16:45 +0500

    added lab 10

---
+++ Lab_10/Task.cpp
@@ -0,0 +1,179 @@
+#include <iostream>
+
+using namespace std;
+
+class Employee
+{
+protected:
+    string name, designation, department;
+    int salary;
+
+public:
+    Employee(
+        string name,
+        string designation,
+        string department,
+        int salary)
+        : name(name),
+          designation(designation),
+          department(department),
+          salary(salary) {}
+
+    bool verify(string department)
+    {
+        return this->department == department;
+    }
+};
+
+class Staff : public Employee
+{
+protected:
+    int overtimeHours;
+
+public:
+    Staff(
+        string name,
+        string designation,
+        string department,
+        int salary,
+        int overtimeHours)
+        : Employee(
+              name,
+              designation,
+              department,
+              salary),
+          overtimeHours(overtimeHours)
+    {
+    }
+
+    bool verify(string department)
+    {
+        return this->department == department;
+    }
+
+    bool verify(int lowerBound, int upperBound)
+    {
+        return this->salary > lowerBound && this->salary < upperBound;
+    }
+};
+
+class Course
+{
+    string code, name, creditHours;
+    bool assigned;
+
+public:
+    Course() {}
+
+    Course(
+        string code,
+        string name,
+        string creditHours)
+        : code(code),
+          name(name),
+          creditHours(creditHours),
+          assigned(false) {}
+
+    string getCode()
+    {
+        return this->code;
+    }
+
+    string getName()
+    {
+        return this->name;
+    }
+
+    string getCreditHours()
+    {
+        return this->creditHours;
+    }
+};
+
+class Faculty : public Employee
+{
+protected:
+    int workingHours, assignedCourseCount;
+    Course courseList[10];
+
+public:
+    Faculty(
+        string name,
+        string designation,
+        string department,
+        int salary,
+        int workingHours)
+        : Employee(
+              name,
+              designation,
+              department,
+              salary),
+          workingHours(workingHours),
+          assignedCourseCount(0)
+    {
+    }
+
+    void assignCourse(Course courseList[], int length)
+    {
+        char targetChar;
+
+        if (this->department == "Computer Science")
+        {
+            targetChar = 'C';
+        }
+        else if (this->department == "Management Science")
+        {
+            targetChar = 'M';
+        }
+        else if (this->department == "Electrical Engineering")
+        {
+            targetChar = 'E';
+        }
+
+        // If valid targetChar, add course
+        if (
+            targetChar == 'C' &&
+            targetChar == 'M' &&
+            targetChar == 'E')
+        {
+
+            // checking for valid course
+            for (int i = 0; i < length; i++)
+            {
+                if (courseList[i].getName()[0] == targetChar)
+                {
+                    // add course
+                    this->courseList[assignedCourseCount] = courseList[i];
+                    assignedCourseCount++;
+
+                    // add credit hours
+                    workingHours += stoi(courseList[i].getCreditHours());
+
+                    if (workingHours > 12)
+                    {
+                        cout << "[WARNING] Working hours exceeded 12." << endl;
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+
+    void printSalary()
+    {
+        cout << "Salary: ";
+
+        if (workingHours <= 36)
+        {
+            cout << salary << endl;
+        }
+        else
+        {
+            salary += (1000 * (salary - 36));
+            cout << salary << endl;
+        }
+    }
+};
+
+int main() {}
